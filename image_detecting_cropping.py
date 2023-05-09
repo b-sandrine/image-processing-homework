cb8c869b5d1857d7ca044873d7f357aa34facfaa
b-sandrine <sanrdushimimana@gmail.com>
2023-05-09 10:27:27 +0200

    Changed saving location

--- image_detecting_cropping.py
+++ image_detecting_cropping.py
@@ -2,7 +2,7 @@
 import cv2
 
 # read the input image
-img = cv2.imread('/home/sandrine/Documents/s6_resources/embedded-systems/python-practice/imageProcessingByInstructor/billie4.png')
+img = cv2.imread('./test-images/billie.jpg')
 
 # convert to grayscale of each frames
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -21,7 +21,7 @@ if len(faces) > 0:
         cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 255), 2)
         face = img[y:y + h, x:x + w]
         cv2.imshow(f"Cropped Face {i}", face)
-        cv2.imwrite(f'face{i}.jpg', face)
+        cv2.imwrite(f'cropped-images/face{i}.jpg', face)
         print(f"face{i}.jpg is saved")
 
 # display the image with detected faces
