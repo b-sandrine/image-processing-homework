bd80acf1f82ff17449e79b1e2ab1fa21149bc309
b-sandrine <sanrdushimimana@gmail.com>
2023-05-09 11:00:53 +0200

    Final touches

--- image_detecting_cropping.py
+++ image_detecting_cropping.py
@@ -2,7 +2,7 @@
 import cv2
 
 # read the input image
-img = cv2.imread('./test-images/billie.jpg')
+img = cv2.imread('./test-images/billie2.jpg')
 
 # convert to grayscale of each frames
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
